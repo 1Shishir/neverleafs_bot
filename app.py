1122bfb4359297f2a4512513888db9a5f38250c6
“Mashal” <imashalk1@gmail.com>
2025-02-18 17:02:12 +0500

    fixed5

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ from modules.orderstatus import router as orderstatus_router
 app = FastAPI()
 
 origins = [
-    "https://chatbottest-production.up.railway.app",  # VERY IMPORTANT
+    "https://chatbottest-production.up.railway.app",  
 ]
 
 
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
