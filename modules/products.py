f2df3c7645a6dd106d674776a28d1a9d8624ec70
“Mashal” <imashalk1@gmail.com>
2025-02-20 13:30:49 +0500

    fixed6

--- modules/products.py
+++ modules/products.py
@@ -5,7 +5,8 @@ import os
 router = APIRouter()
 
 # Base URL for serving images
-BASE_IMAGE_URL = os.getenv("BASE_IMAGE_URL", "http://127.0.0.1:8000/static/images/")
+BASE_IMAGE_URL = os.getenv("BASE_IMAGE_URL", "https://chatbottest-production.up.railway.app/static/images/")
+
 
 
 # Updated product data with Dutch names, heights, and categories
